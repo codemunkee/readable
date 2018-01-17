4bee0fff5b032d982eb77d128c3858c659e88d4a
Russ Nealis <codemunkee@gmail.com>
2018-01-16 18:10:48 -0800

    boiler plate postview

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import IconArrowUp from './IconArrowUp.svg';
+import IconArrowDown from './IconArrowDown.svg';
+import './PostView.css';
 
 class PostView extends Component {
   render()  {
+    console.log('PROPS', this.props)
     const postID = this.props.match.params.id;
     const post = this.props.posts[postID];
-    return (
-      <div>
-        <h1>{post.title}</h1>
-        <p>{post.body}</p>
-      </div>
-    )
+    if (!post) {
+      return (
+        <h1>Loading...</h1>
+      )
+    } else {
+      return (
+        <div className="PostView">
+          <div className="PostView-title">
+            <span>
+              <img src={IconArrowUp} className="PostInfo-logo" alt="arrow up" onClick={this.handleUpVote}/>
+              <img src={IconArrowDown} className="PostInfo-logo" alt="arrow down" onClick={this.handleDownVote} />
+              {post.title}
+            </span>
+            <div className="PostView-subtitle">
+              <span>{post.voteScore} votes by poster 2 minutes ago | <span onClick={this.handleRemove}>remove</span></span>
+            </div>
+          </div>
+          <div className="PostView-body">
+            <p>
+              {post.body}
+            </p>
+          </div>
+        </div>
+      )
+    }
   }
 }
 
