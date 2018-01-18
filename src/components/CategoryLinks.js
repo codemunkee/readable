45911144bdf10bb3dcd4580a60cd2d20de1ff094
Russ Nealis <codemunkee@gmail.com>
2018-01-18 08:51:05 -0800

    fix key error - styling still broken

--- src/components/CategoryLinks.js
+++ src/components/CategoryLinks.js
@@ -8,7 +8,7 @@ class CategoryLinks extends Component {
     return (
       <div className="CategoryLinks">
         { this.props.categories.items.map(item =>
-           <Link to={'/category/' + item.path}>{item.name}</Link>
+           <Link key={item.name} to={'/category/' + item.path}>{item.name}</Link>
         )}
       </div>
     )
