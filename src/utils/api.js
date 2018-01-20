5a251f4beda90d33ec44e4312635a32e41d04da8
Russ Nealis <codemunkee@gmail.com>
2018-01-20 05:55:04 -0800

    added api method for retrieving post comments

--- src/utils/api.js
+++ src/utils/api.js
@@ -124,3 +124,24 @@ export function removePost(postID) {
       return {};
     });
 }
+
+export function fetchComments(postID) {
+  /* Retrieve all comments from the API for a particular postID */
+  const init = Object.assign({}, stockInit, {'method': 'GET'})
+
+  return fetch('http://localhost:3001/posts/' + postID + '/comments', init)
+    .then(resp => resp.json())
+    .then(resp => {
+      // convert our response of an array of objects to an object of objects,
+      // the latter is the preferred way of describing state in redux
+      const reduxified = resp.reduce((accumulator, item) => {
+        accumulator[item.id] = item;
+        return accumulator
+      }, {})
+      return reduxified;
+    })
+    .catch(error => {
+      console.log('Error: Unable to retrieve posts.', error);
+      return {};
+    });
+}
