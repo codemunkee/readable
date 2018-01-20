c64bb2a42059e9cfc8abfdc736eb5554a5ff6223
Russ Nealis <codemunkee@gmail.com>
2018-01-19 18:42:29 -0800

    filter posts to a specific category

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,30 +7,47 @@ import './PostList.css';
 
 class PostList extends Component {
 
-  render() {
-    const { items } = this.props.posts
+  composePosts() {
+    // we perform a handful of operations on posts before they're
+    // rendered. namely, putting them in an array, numbering them,
+    // filtering them to a specific category (if required), and
+    // sorting them (if required)
+    const { items } = this.props.posts;
 
+    // put the posts into an array
+    let posts = Object.keys(items).map(postID => {
+      return this.props.posts.items[postID];
+    })
+
+    // if a category is defined filter them to only that category
+    if (this.props.category) {
+      posts = posts.filter(post => post.category === this.props.category);
+    }
+
+    return this.numberPosts(posts)
+  }
+
+  numberPosts(posts) {
+    // add numbers to the posts
+    for (let i = 0; i < posts.length; i++) {
+      posts[i] = { ...posts[i], 'number': i + 1 }
+    }
+    return posts;
+  }
+
+  render() {
     if (this.props.posts.isFetching) {
       return (<h1>Fetching</h1>)
     } else {
-      const numberedPosts = addPostNumber(items);
       return (
         <div className="PostList">
-          { numberedPosts.map(post => <PostInfo key={post.id} post={post} />) }
+          { this.composePosts().map(post =>
+            <PostInfo key={post.id} post={post} />)
+          }
         </div>
       )
     }
   }
-}
-
-function addPostNumber(posts) {
-  // we add a property with a sort number to each post as
-  // just a convenience thing
-  let count = 1;
-  return Object.keys(posts).map(postId => {
-    const newPost = { ...posts[postId], 'number': count++}
-    return newPost;
-  })
 }
 
 function mapStateToProps(state) {
