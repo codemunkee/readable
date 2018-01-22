3c209e8bfda855600d43cf1b9618274ef250e3c0
Russ Nealis <codemunkee@gmail.com>
2018-01-22 09:12:08 -0800

    some styling for sort arrows

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import './App.css';
 import AppTitle from './AppTitle';
 import AppMenu from './AppMenu';
--- src/components/AppMenu.js
+++ src/components/AppMenu.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import IconArrowUp from './IconArrowUp.svg';
+import IconArrowDown from './IconArrowDown.svg';
 import './AppMenu.css';
 
 class AppMenu extends Component {
+
+  handleSort() {
+    console.log('oh boy')
+  }
+
   render() {
     return (
       <div className="AppMenu">
-        <Link to="/submit">submit</Link>
+        <li><Link to="/submit">submit</Link></li>
         &nbsp;|&nbsp;
-        <a role="button" onClick={this.handleSort}>sort</a>
+        <li>
+            <a role="button" onClick={this.handleSort}>sort
+              <ul>
+                <li>date
+                  <div className="AppMenu-arrow-box">
+                    <img src={IconArrowUp} className="AppMenu-arrow" alt="arrow up" onClick={this.handleUpVote}/>
+                    <img src={IconArrowDown} className="AppMenu-arrow" alt="arrow down" onClick={this.handleDownVote} />
+                  </div>
+                </li>
+                <li>votes
+                  <div className="AppMenu-arrow-box">
+                    <img src={IconArrowUp} className="AppMenu-arrow" alt="arrow up" onClick={this.handleUpVote}/>
+                    <img src={IconArrowDown} className="AppMenu-arrow" alt="arrow down" onClick={this.handleDownVote} />
+                  </div>
+                </li>
+              </ul>
+            </a>
+        </li>
       </div>
     )
   }
