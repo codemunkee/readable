61ba78a34a018b0865a01f2406824c9ec0521de9
Russ Nealis <rnealis@twilio.com>
2018-01-27 09:12:55 -0800

    use short-circuit conditional for post views

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -9,65 +9,71 @@ import { incrementPostVotes, decrementPostVotes, deletePost } from '../actions';
 import './PostView.css';
 
 class PostView extends Component {
-  state = {
-    postID: this.props.match.params.id
-  }
 
   handleUpVote = data => {
-    this.props.upVote(this.state.postID);
+    this.props.upVote(this.props.match.params.id);
   }
 
   handleDownVote = data => {
-    this.props.downVote(this.state.postID);
+    this.props.downVote(this.props.match.params.id);
   }
 
   handleRemove = data => {
-    this.props.removePost(this.state.postID);
+    this.props.removePost(this.props.match.params.id);
   }
 
   render()  {
-    if (this.props.posts.isFetching) {
-      return (
-        <h1>Loading...</h1>
-      )
-    } else {
-      const postID = this.props.match.params.id;
-      const post = this.props.posts.items[postID];
-      const humanTime = new Date(post.timestamp).toLocaleString('en-US');
-      return (
-        <div className="PostView">
-          <div className="PostView-title">
-            <span>
-              <img src={IconArrowUp}
-                   className="PostInfo-logo"
-                   alt="arrow up"
-                   onClick={this.handleUpVote}/>
-              <img src={IconArrowDown}
-                   className="PostInfo-logo"
-                   alt="arrow down"
-                   onClick={this.handleDownVote} />
-              {post.title}
-            </span>
-            <div className="PostView-subtitle">
-              <span>{post.voteScore} votes by poster {humanTime}</span>
-              <Link to={'/post/' + postID + '/edit'}> edit</Link>
-               &nbsp;|&nbsp;
-              <a role="button" onClick={this.handleRemove}>remove</a>
+    const postID = this.props.match.params.id;
+    const post = this.props.posts.items[postID];
+    const humanTime = (post)
+                        ? new Date(post.timestamp).toLocaleString('en-US')
+                        : undefined;
+
+    return (
+      <div>
+        { this.props.posts.isFetching &&
+          <h1>Fetching Post info...</h1> }
+
+        { !this.props.posts.isFetching &&
+          !post &&
+          <h2>Post not found :(</h2> }
+
+        { !this.props.posts.isFetching &&
+          post &&
+          <div className="PostView">
+            <div className="PostView-title">
+              <span>
+                <img src={IconArrowUp}
+                     className="PostInfo-logo"
+                     alt="arrow up"
+                     onClick={this.handleUpVote}/>
+                <img src={IconArrowDown}
+                     className="PostInfo-logo"
+                     alt="arrow down"
+                     onClick={this.handleDownVote} />
+                {post.title}
+              </span>
+              <div className="PostView-subtitle">
+                <span>{post.voteScore} votes by poster {humanTime}</span>
+                <Link to={'/post/' + postID + '/edit'}> edit</Link>
+                 &nbsp;|&nbsp;
+                <a role="button" onClick={this.handleRemove}>remove</a>
+              </div>
+            </div>
+            <div className="PostView-body">
+              <p>
+                {post.body}
+              </p>
+              <CommentSubmit postID={postID} />
+            </div>
+            <div>
+              <hr/>
+              <CommentList postID={postID} />
             </div>
           </div>
-          <div className="PostView-body">
-            <p>
-              {post.body}
-            </p>
-            <CommentSubmit postID={postID} />
-          </div>
-          <div>
-            <hr/>
-            <CommentList postID={postID} />
-          </div>
-        </div>
-      )
-    }
+        }
+      </div>
+    )
   }
 }
 
