cd0a93e3873a7957e29592f59417249e324ae13d
Russ Nealis <rnealis@twilio.com>
2018-01-29 09:33:28 -0800

    fixed linting errors on CommentEdit

--- src/components/CommentEdit.js
+++ src/components/CommentEdit.js
@@ -5,34 +5,35 @@ import { putComment, fetchComments } from '../actions';
 import './CommentEdit.css';
 
 class CommentEdit extends Component {
-
   state = {
     id: '',
     body: '',
-    fireRedirect: false
-  }
-
-  componentWillReceiveProps(newProps) {
-    const comment = newProps.comments.items[this.props.match.params.commentid];
-    if (comment) {
-      this.setState({id: comment.id, body: comment.body})
-    }
+    fireRedirect: false,
   }
 
   componentDidMount() {
     this.props.fetchComments(this.props.match.params.postid);
   }
 
-  handleChange = event => {
-    this.setState({[event.target.name]: event.target.value});
-    mapStateToProps(this.state)
+  componentWillReceiveProps(newProps) {
+    const comment = newProps.comments.items[this.props.match.params.commentid];
+    if (comment) {
+      this.setState({ id: comment.id, body: comment.body });
+    }
   }
 
-  handleSubmit = event => {
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+    mapStateToProps(this.state); // eslint-disable-line no-use-before-define
+  }
+
+  handleSubmit = (event) => {
     event.preventDefault();
-    this.props.editComment({'id': this.state.id,
-                            'body': this.state.body});
-    this.setState({fireRedirect: true});
+    this.props.editComment({
+      id: this.state.id,
+      body: this.state.body,
+    });
+    this.setState({ fireRedirect: true });
   }
 
   render() {
@@ -40,51 +41,58 @@ class CommentEdit extends Component {
     const parentPost = this.props.posts.items[this.props.match.params.postid];
     return (
       <div>
-      { this.props.comments.isFetching &&
-        <h2>Fetching Comment Info</h2> }
+        { this.props.comments.isFetching &&
+          <h2>Fetching Comment Info</h2> }
 
-      { !this.props.comments.isFetching &&
-        !comment &&
-        <h2>Comment not found :(</h2> }
+        { !this.props.comments.isFetching &&
+          !comment &&
+          <h2>Comment not found :(</h2> }
 
-      { !this.props.comments.isFetching &&
-        comment &&
-        <div>
-        <form className="CommentEdit" onSubmit={this.handleSubmit} >
-          <fieldset>
-            <Link to={'/post/' + parentPost.id}>{parentPost.title}</Link>
-            <div className="CommentEdit-text">
-              <textarea name="body"
-                        value={this.state.body}
-                        onChange={this.handleChange} />
-              <br/>
-            </div>
-          </fieldset>
-          <button className="CommentEdit-button"
-                  type="submit"
-                  value="submit">Edit Comment
-          </button>
-        </form>
-        { this.state.fireRedirect && <Redirect to={'/post/' + parentPost.id} />}
-        </div>
-      }
+        { !this.props.comments.isFetching &&
+          comment &&
+          <div>
+            <form
+              className="CommentEdit"
+              onSubmit={this.handleSubmit}
+            >
+              <fieldset>
+                <Link to={`/post/${parentPost.id}`}>{parentPost.title}</Link>
+                <div className="CommentEdit-text">
+                  <textarea
+                    name="body"
+                    value={this.state.body}
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </fieldset>
+              <button
+                className="CommentEdit-button"
+                type="submit"
+                value="submit"
+              >
+                Edit Comment
+              </button>
+            </form>
+            { this.state.fireRedirect && <Redirect to={`/post/${parentPost.id}`} />}
+          </div>
+        }
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps (state) {
-  return state
+function mapStateToProps(state) {
+  return state;
 }
 
-function mapDispatchToProps (dispatch) {
+function mapDispatchToProps(dispatch) {
   return {
-    editComment: (data) => dispatch(putComment(data)),
-    fetchComments: (postID) => dispatch(fetchComments(postID)),
-  }
+    editComment: data => dispatch(putComment(data)),
+    fetchComments: postID => dispatch(fetchComments(postID)),
+  };
 }
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
-)(CommentEdit)
+  mapDispatchToProps,
+)(CommentEdit);
