3f679f09d607a53b72824c4211f5047002c7bf9e
Russ Nealis <rnealis@twilio.com>
2018-01-29 09:01:30 -0800

    fixed linting on PostEdit

--- src/components/PostEdit.js
+++ src/components/PostEdit.js
@@ -5,41 +5,45 @@ import { putPost, fetchPosts } from '../actions';
 import './PostEdit.css';
 
 class PostEdit extends Component {
-
   state = {
     id: '',
     title: '',
     body: '',
     category: '',
-    fireRedirect: false
-  }
-
-  componentWillReceiveProps(newProps) {
-    const post = this.props.posts.items[this.props.match.params.id];
-    if (post) {
-      this.setState({id: post.id,
-                     title: post.title,
-                     body: post.body,
-                     category: post.category})
-    }
+    fireRedirect: false,
   }
 
   componentDidMount() {
     this.props.fetchPosts();
   }
 
-  handleChange = event => {
-    this.setState({[event.target.name]: event.target.value});
-    mapStateToProps(this.state)
+  componentWillReceiveProps() {
+    const post = this.props.posts.items[this.props.match.params.id];
+    if (post) {
+      this.setState({
+        id: post.id,
+        title: post.title,
+        body: post.body,
+        category: post.category,
+      });
+    }
   }
 
-  handleSubmit = event => {
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+    // eslint-disable-next-line no-use-before-define
+    mapStateToProps(this.state);
+  }
+
+  handleSubmit = (event) => {
     event.preventDefault();
-    this.props.editPost({'id': this.state.id,
-                         'title': this.state.title,
-                         'category': this.state.category,
-                         'body': this.state.body});
-    this.setState({fireRedirect: true, russ: 'oh hyea'})
+    this.props.editPost({
+      id: this.state.id,
+      title: this.state.title,
+      category: this.state.category,
+      body: this.state.body,
+    });
+    this.setState({ fireRedirect: true });
   }
 
   render() {
@@ -47,73 +51,86 @@ class PostEdit extends Component {
     const post = this.props.posts.items[this.props.match.params.id];
     return (
       <div>
-      { this.props.posts.isFetching &&
-        this.props.categories.isFetching &&
-        <h2>Fetching Post Info</h2> }
+        { this.props.posts.isFetching &&
+          this.props.categories.isFetching &&
+          <h2>Fetching Post Info</h2> }
 
-      { !this.props.posts.isFetching &&
-        !this.props.categories.isFetching &&
-        !post &&
-        <h2>Post not found :(</h2> }
+        { !this.props.posts.isFetching &&
+          !this.props.categories.isFetching &&
+          !post &&
+          <h2>Post not found :(</h2> }
 
-      { !this.props.posts.isFetching &&
-        !this.props.categories.isFetching &&
-        post &&
-        <div>
-        <form className="PostEdit" onSubmit={this.handleSubmit} >
-          <fieldset>
-            <label className="PostEdit-title">title</label>
-            <input name="title"
-                   type="text"
-                   value={this.state.title}
-                   onChange={this.handleChange} />
-            <br/>
+        { !this.props.posts.isFetching &&
+          !this.props.categories.isFetching &&
+          post &&
+          <div>
+            <form className="PostEdit" onSubmit={this.handleSubmit} >
+              <fieldset>
+                <label htmlFor="title" className="PostEdit-title">
+                  <span>title</span>
+                  <input
+                    name="title"
+                    type="text"
+                    value={this.state.title}
+                    onChange={this.handleChange}
+                  />
+                </label>
+                <label htmlFor="category">
+                  <span>category</span>
+                  <select name="category" value={post.category} onChange={this.handleChange}>
+                    { categories.map(category => (
+                      <option
+                        key={category.name}
+                        value={category.name}
+                      >
+                        {category.name}
+                      </option>))
+                    }
+                  </select>
+                </label>
 
-            <label>category</label>
-            <select name="category" value={post.category} onChange={this.handleChange}>
-            { categories.map(category =>
-              <option key={category.name}
-                      value={category.name}>
-                      {category.name}
-              </option>)
-            }
-            </select>
-
-            <div className="PostEdit-text">
-              <label>body</label>
-              <textarea name="body"
-                        value={this.state.body}
-                        onChange={this.handleChange} />
-              <br/>
-            </div>
-          </fieldset>
-          <button className="PostEdit-button"
-                  type="submit"
-                  value="submit">Update Post
-          </button>
-        </form>
-        {this.state.fireRedirect && (
-          <Redirect to={'/post/' + post.id}/>
-        )}
-        </div>
-      }
+                <div className="PostEdit-text">
+                  <label htmlFor="body">
+                    <span>body</span>
+                    <textarea
+                      name="body"
+                      value={this.state.body}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </div>
+              </fieldset>
+              <button
+                className="PostEdit-button"
+                type="submit"
+                value="submit"
+              >Update Post
+              </button>
+            </form>
+            {this.state.fireRedirect && (
+              <Redirect
+                to={`/post/${post.id}`}
+              />
+            )}
+          </div>
+        }
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps (state) {
-  return state
+function mapStateToProps(state) {
+  return state;
 }
 
-function mapDispatchToProps (dispatch) {
+function mapDispatchToProps(dispatch) {
   return {
-    editPost: (data) => dispatch(putPost(data)),
+    editPost: data => dispatch(putPost(data)),
     fetchPosts: () => dispatch(fetchPosts()),
-  }
+  };
 }
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
-)(PostEdit)
+  mapDispatchToProps,
+)(PostEdit);
