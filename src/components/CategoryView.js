58d276062f6ec83d45b095ff06b13db5fff4652a
Russ Nealis <rnealis@twilio.com>
2018-01-28 18:50:48 -0800

    Updated CategoryView to be a functional component

--- src/components/CategoryView.js
+++ src/components/CategoryView.js
@@ -1,20 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PostList from './PostList';
 
-class CategoryView extends Component {
-  render() {
-    const categoryName = this.props.match.params.id;
-    return (
-      <PostList category={categoryName} />
-    )
-  }
+function CategoryView(props) {
+  const categoryName = props.match.params.id;
+  return <PostList category={categoryName} />;
 }
 
 function mapStateToProps(state) {
   return state;
 }
 
-export default connect(
-  mapStateToProps
-)(CategoryView)
+export default connect(mapStateToProps)(CategoryView);
