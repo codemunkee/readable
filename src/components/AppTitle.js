3440ac40b341a2da0583351bfef1f6f9472002a9
Russ Nealis <rnealis@twilio.com>
2018-01-29 19:55:37 -0800

    allow linking back home when page title is Readable

--- src/components/AppTitle.js
+++ src/components/AppTitle.js
@@ -5,14 +5,25 @@ import IconR from './IconR.svg';
 /* Shows the Icon and the Title of each page */
 
 export default function AppTitle(props) {
+  // if the name of the title is Readable, we link back to root
+  const linkable = (props.name === 'Readable');
   return (
     <div className="App-title">
       <Link to="/">
         <img className="App-logo" src={IconR} alt="icon" />
       </Link>
-      <span className="App-title">
-        { props.name }
-      </span>
+      { linkable && (
+        <Link to="/">
+          <span className="App-title">
+            { props.name }
+          </span>
+        </Link>
+      )}
+      { !linkable && (
+        <span className="App-title">
+          { props.name }
+        </span>
+      )}
     </div>
   );
 }
