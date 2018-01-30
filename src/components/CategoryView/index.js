fbab1f9e794eaa6fcfb6a0cb39fb0e811113560c
Russ Nealis <rnealis@twilio.com>
2018-01-30 08:40:17 -0800

    dont map state to categoryview, doesnt need it

--- src/components/CategoryView/index.js
+++ src/components/CategoryView/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import PostList from '../PostList/';
 
-function CategoryView(props) {
+export default function CategoryView(props) {
   const categoryName = props.match.params.id;
   return <PostList category={categoryName} />;
 }
-
-function mapStateToProps(state) {
-  return state;
-}
-
-export default connect(mapStateToProps)(CategoryView);
