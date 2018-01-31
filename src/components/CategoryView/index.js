8fb17dece707bea0ba420d6cff14f7d53c3366fb
Russ Nealis <rnealis@twilio.com>
2018-01-31 07:54:54 -0800

    change CategoryView back to original setup

--- src/components/CategoryView/index.js
+++ src/components/CategoryView/index.js
@@ -3,10 +3,5 @@ import PostList from '../PostList/';
 
 export default function CategoryView(props) {
   const categoryName = props.match.params.category;
-  return (
-    <div>
-      { categoryName === 'submit' && <h1>Hello</h1> }
-      <PostList category={categoryName} />
-    </div>
-  );
+  return <PostList category={categoryName} />;
 }
