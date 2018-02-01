9a2b77d0bba44aeebe84e74888763ecebdf3ad06
Russ Nealis <rnealis@twilio.com>
2018-01-31 20:47:39 -0800

    change AppMenu into a functional component

--- src/components/AppMenu/index.js
+++ src/components/AppMenu/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import IconArrowUp from '../IconArrows/IconArrowUp.svg';
@@ -6,70 +6,49 @@ import IconArrowDown from '../IconArrows/IconArrowDown.svg';
 import { updateSort } from '../../actions';
 import './AppMenu.css';
 
-class AppMenu extends Component {
-  handleSortClick(sortType) {
-    this.props.updateSort(sortType);
+function AppMenu(props) {
+  function handleSortClick(sortType) {
+    props.updateSort(sortType);
   }
 
-  render() {
-    const { sortSetting } = this.props;
+  function sortingButton(iconName, sortSettingString, imageAlt) {
     return (
-      <div className="AppMenu">
-        <li><Link to="/submit">submit</Link></li>
-        &nbsp;|&nbsp;
-        <li>
-          <a role="button">sort
-            <ul>
-              <li>date
-                <div className="AppMenu-arrow-box">
-                  <img
-                    src={IconArrowDown}
-                    style={(sortSetting === 'dateDesc') ?
-                      { backgroundColor: 'yellow' } :
-                      { backgroundColor: '#f6f6ef' }}
-                    className="AppMenu-arrow "
-                    alt="arrow down"
-                    onClick={() => this.handleSortClick('dateDesc')}
-                  />
-                  <img
-                    src={IconArrowUp}
-                    style={(sortSetting === 'dateAsc') ?
-                      { backgroundColor: 'yellow' } :
-                      { backgroundColor: '#f6f6ef' }}
-                    className="AppMenu-arrow"
-                    alt="arrow up"
-                    onClick={() => this.handleSortClick('dateAsc')}
-                  />
-                </div>
-              </li>
-              <li>votes
-                <div className="AppMenu-arrow-box">
-                  <img
-                    src={IconArrowDown}
-                    style={(sortSetting === 'votesDesc') ?
-                      { backgroundColor: 'yellow' } :
-                      { backgroundColor: '#f6f6ef' }}
-                    className="AppMenu-arrow"
-                    alt="arrow down"
-                    onClick={() => this.handleSortClick('votesDesc')}
-                  />
-                  <img
-                    src={IconArrowUp}
-                    style={(sortSetting === 'votesAsc') ?
-                      { backgroundColor: 'yellow' } :
-                      { backgroundColor: '#f6f6ef' }}
-                    className="AppMenu-arrow"
-                    alt="arrow up"
-                    onClick={() => this.handleSortClick('votesAsc')}
-                  />
-                </div>
-              </li>
-            </ul>
-          </a>
-        </li>
-      </div>
+      <img
+        src={iconName}
+        style={(props.sortSetting === sortSettingString) ?
+          { backgroundColor: 'yellow' } :
+          { backgroundColor: '#f6f6ef' }}
+        className="AppMenu-arrow "
+        alt={imageAlt}
+        onClick={() => handleSortClick(sortSettingString)}
+      />
     );
   }
+
+  return (
+    <div className="AppMenu">
+      <li><Link to="/submit">submit</Link></li>
+      &nbsp;|&nbsp;
+      <li>
+        <a role="button">sort
+          <ul>
+            <li>date
+              <div className="AppMenu-arrow-box">
+                {sortingButton(IconArrowDown, 'dateDesc', 'arrow down')}
+                {sortingButton(IconArrowUp, 'dateAsc', 'arrow up')}
+              </div>
+            </li>
+            <li>votes
+              <div className="AppMenu-arrow-box">
+                {sortingButton(IconArrowDown, 'votesDesc', 'arrow down')}
+                {sortingButton(IconArrowUp, 'votesAsc', 'arrow up')}
+              </div>
+            </li>
+          </ul>
+        </a>
+      </li>
+    </div>
+  );
 }
 
 function mapStateToProps({ sortSettings }) {
